dae77a903544e6c13f3fb2295f1b062e7e425afd
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-02 14:01:30 -0400

    center event holder and add react-google-map script

--- app/assets/javascripts/components/MyFolders.jsx
+++ app/assets/javascripts/components/MyFolders.jsx
@@ -1,7 +1,7 @@
 var MyFolders = React.createClass({
   render: function() {
     return (
-      <div className="col-sm-3 profile-info">
+      <div className="col-sm-1 profile-info">
       </div>
     );
   }
